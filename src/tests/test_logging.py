2ce797dbe3a6cb664ac9bd1ef3b4f494b0327435
NikLain99 <nkutsakivskyi@gmail.com>
2026-03-19 19:25:30 +0100

    logging has been created for this project

--- src/tests/test_logging.py
+++ src/tests/test_logging.py
@@ -1,9 +1,7 @@
-from config.logging_config import setup_logging
+from src.infrastructure.logging import setup_logging
 import uuid
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 batch_id = str(uuid.uuid4())
 logger = setup_logging(batch_id=batch_id)
